ebdc68572020c3a6dc6758f2a62812a1b6033ee0
SWJ <tjdnwkd99@naver.com>
2022-06-07 15:03:27 +0900

    덧셈함수 완성

--- src/js/arithmaticOperations.js
+++ src/js/arithmaticOperations.js
@@ -1,3 +1,7 @@
 /*
-* 이 곳에 사칙연산에 관한 함수 모듈들을 작성합니다.
-*/
+ * 이 곳에 사칙연산에 관한 함수 모듈들을 작성합니다.
+ */
+export const getAddition = (operand1, operand2) => {
+  let sum = operand1 + operand2;
+  return sum;
+};
